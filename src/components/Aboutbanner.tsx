14b6fe85a48af23a09356257c55807481e2aa49f
Aswin Sanosh <aswinsanosh97@gmail.com>
2025-07-27 20:11:51 +0530

    a

--- src/components/Aboutbanner.tsx
+++ src/components/Aboutbanner.tsx
@@ -48,7 +48,7 @@ export default function Aboutbanner() {
           As a startup driven by innovation and agility, we craft tailored digital solutions
           that solve real problems and unlock new opportunities. From building powerful digital
           experiences to scaling your ideas, we&#39;re here to support your journey every step of
-          the way. Our mission is simple to empower businesses worldwide with the tools,
+          the way. Our mission is simple - to empower businesses worldwide with the tools,
           technology, and creativity they need to grow.
         </p>
 
