634a377c3c4b542391039cdd915e3d56b437989d
Aswin Sanosh <aswinsanosh97@gmail.com>
2025-07-27 13:55:41 +0530

    logo update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,7 +58,7 @@ export default function Navbar() {
               width={100}
               height={100}
               loading="lazy"
-              className="lg:h-14 w-auto h-6"
+              className="lg:h-10 w-auto h-6"
             />
           </Link>
         </div>
@@ -74,7 +74,7 @@ export default function Navbar() {
         >
           <div
             className={`
-              z-20 top-8 py-2 px-5 shadow-lg shadow-black/30 rounded-lg
+              z-20 top-8 py-2 px-5 shadow-lg shadow-black/30 rounded-lg backdrop-blur-2xl
               flex items-center justify-between gap-16
               font-poppins text-white text-lg font-light
               text-center whitespace-nowrap flex-nowrap
