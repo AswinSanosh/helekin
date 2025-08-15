e7f425ac007b1acc3ad1f6219292f0bcc3befbc3
Aswin Sanosh <aswinsanosh97@gmail.com>
2025-08-16 00:24:28 +0530

    loading

--- src/app/(root)/about/loading.tsx
+++ src/app/(root)/about/loading.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
+
 export default function Loading() {
-    return (
-        <div className="flex items-center justify-center h-screen bg-[#030303]">
-        <div className="animate-spin">
-            <Image width={60} height={60} src="/svg/loading.svg" alt="Loading" className="h-full w-full" />
-        </div>
-        </div>
-        
-    );
-    }
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#030303]">
+      <div className="animate-spin">
+        <Image
+          width={170} // reduced from 60
+          height={170}
+          src="/svg/loading.svg"
+          alt="Loading"
+        />
+      </div>
+    </div>
+  );
+}
